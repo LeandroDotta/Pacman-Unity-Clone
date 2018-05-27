e1bc5d055d0bc8f7431434091ef02969b424aa19
DragonSkyX <dragonskyx@gmail.com>
2018-05-27 00:49:47 -0300

    Adição do Script do contador de Score/Vida

---
+++ Assets/Scripts/UIControl.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIControl : MonoBehaviour {
+
+
+    public static UIControl Instance { get; private set; }
+    public Text score;
+    public Text lives;
+
+
+    void Start () {
+
+        if (Instance == null)
+            Instance = this;
+        else if (Instance != this)
+            Instance = null;
+
+        score.GetComponent<Text>();
+        lives.GetComponent<Text>();
+    }
+	
+
+	void LateUpdate () {
+
+        score.text = "Score:" + GameManager.Instance.score;
+        lives.text = "Lives:" + GameManager.Instance.lifes;
+
+
+    }
+}
