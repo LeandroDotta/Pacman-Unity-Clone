b158dde8b0186d8f480e21d636b56e58adfcc867
LeandroDotta <leandro.dotta@yahoo.com.br>
2018-05-27 01:02:24 -0300

    Adicionado UIControl ao canvas para alterar os textos de pontução e vida

--- Assets/Scripts/UIControl.cs
+++ Assets/Scripts/UIControl.cs
@@ -3,25 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class UIControl : MonoBehaviour {
-
-
-    public static UIControl Instance { get; private set; }
+public class UIControl : MonoBehaviour 
+{
     public Text score;
     public Text lives;
-
-
-    void Start () {
-
-        if (Instance == null)
-            Instance = this;
-        else if (Instance != this)
-            Instance = null;
-
-        score.GetComponent<Text>();
-        lives.GetComponent<Text>();
-    }
-	
 
 	void LateUpdate () {
 
